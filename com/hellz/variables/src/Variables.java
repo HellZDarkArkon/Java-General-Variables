4d169eddab7a90e514bae57d8758993e5fbbc2ea
HellZDarkArkon <maximumwerewolf@gmail.com>
2023-08-25 03:14:31 -0400

    Variables

---
+++ com/hellz/variables/src/Variables.java
@@ -0,0 +1,19 @@
+package hellz.variables.src;
+
+public class Variables {
+    public static void main(String[] args) {
+        int i = 0;
+        float f = 0.1f;
+        double d = 0.00001;
+        char c = 'c';
+        String str = "Hello";
+        boolean b = true;
+
+        System.out.println(str);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(i);
+        System.out.println(b);
+    }
+}
